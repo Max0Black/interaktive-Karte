a6a162d803314dcfa344d8f07706c652ca5b8817
Max0Black <maximilian-ronaldo1@web.de>
2019-07-16 21:42:55 +0200

    Bewegliches Objekt eingefügt

--- Frontend/src/app/map/map.component.ts
+++ Frontend/src/app/map/map.component.ts
@@ -7,15 +7,30 @@ import { Component } from '@angular/core';
 })
 export class MapComponent {
 
+  ein = false;
+  offsetX = 0;
+  offsetY = 0;
+
   constructor() { }
 
   doClick(event) {
-
     let x = event.x;
     let y = event.y;
-    let offsetX = event.offsetX;
-    let offsetY = event.offsetY;
-    console.log(offsetX);
-    console.log(offsetY);
+    this.offsetX = event.offsetX;
+    this.offsetY = event.offsetY;
+    console.log(this.offsetX);
+    console.log(this.offsetY);
+
+    this.ein = true;
+  }
+
+  getStyle() {
+    let tsStyle = {
+      'position': 'absolute',
+      'left': this.offsetX + 'px',
+      'top': this.offsetY + 'px'
+    };
+
+    return tsStyle;
   }
 }
